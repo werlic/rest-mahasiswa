99f96b0af7a369f5a529619a57482e71800d55e3
werlic <agussulaeman453@yahoo.co.id>
2021-06-24 18:49:55 +0700

    Add view data mahasiswa

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Mahasiswa;
 use Illuminate\Http\Request;
 
 class MahasiswaController extends Controller
 {
-    //
+    public function __construct() 
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        $mahasiswa = Mahasiswa::all();
+        return view('mahasiswa.index', ['mahasiswa' => $mahasiswa]);
+    }
+
+    public function create()
+    {
+        return view('mahasiswa.add');
+    }
+
+    public function store(Request $request)
+    {
+        # code...
+    }
+
+    public function edit(Mahasiswa $mahasiswa)
+    {
+        return view('mahasiswa.edit', ['mahasiswa' => $mahasiswa]);
+    }
 }
